2425984508ef79f82094db985e7cf78877eef71a
jdoubleeyun <anjeeyun0767@gmail.com>
2021-11-22 21:17:44 +0900

    Initial commit

---
+++ src/main/java/com/sparta/buildingcommunitylogin/BuildingCommunityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.buildingcommunitylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingCommunityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingCommunityLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/buildingcommunitylogin/BuildingCommunityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.buildingcommunitylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingCommunityLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
